d5e5239e7a60034e7a180e7c1d085ec965ba7820
jasonlam510 <79464531+jasonlam510@users.noreply.github.com>
2026-04-13 21:14:56 +0800

    codegen: fix prettier

--- src/components/dark-mode-toggle.tsx
+++ src/components/dark-mode-toggle.tsx
@@ -19,8 +19,14 @@ export function DarkModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <Sun
+            className="scale-100 rotate-0 transition-all dark:scale-0
+              dark:-rotate-90"
+          />
+          <Moon
+            className="absolute scale-0 rotate-90 transition-all dark:scale-100
+              dark:rotate-0"
+          />
           <span className="sr-only">{t('toggle')}</span>
         </Button>
       </DropdownMenuTrigger>
